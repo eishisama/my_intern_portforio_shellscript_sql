a797149a98b30b005230d58fc8c335b99ac443aa
eishi <102033106+eishisama@users.noreply.github.com>
2023-01-19 23:34:44 +0900

    Update tf_kikaku_change_check.sql

--- tf_kikaku_change_check.sql
+++ tf_kikaku_change_check.sql
@@ -8,66 +8,66 @@ SELECT
   ,A.WAKUSTATUS   
 FROM
   (SELECT DISTINCT
-					T1.HOSOWAKUID         
-					,T1.HOSOWAKURIREKIID  
-					,T1.CURRENTFLAG       
-					,T1.KIKAKUID         
-					,T1.HOSODANID         
-					,T1.TYPEID           
-					,T2.WAKUSTATUS        
-			FROM
-				 HOSOWAKU T1
-			FULL OUTER JOIN
-			  WAKUINFO T2
-			ON
-			  T2.HOSOWAKUID = T1.HOSOWAKUID
-			AND
-			  T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
-			WHERE
-			  T1.UPDATEDATE >= (sysdate - INTERVAL '1' HOUR)
-			AND
-			  T2.WAKUSTATUS = 7
-			ORDER BY
-					T1.HOSOWAKUID
-					,T1.HOSOWAKURIREKIID)A
+ 　　　　　　　　T1.HOSOWAKUID         
+   　　　　,T1.HOSOWAKURIREKIID  
+   　　　　,T1.CURRENTFLAG       
+　　　　　　　　　　,T1.KIKAKUID         
+　　　　　　　　　　,T1.HOSODANID         
+　　　　　　　　　　,T1.TYPEID           
+　　　　　　　　　　,T2.WAKUSTATUS        
+　　　　　　FROM
+　　　　　　　　　　　HOSOWAKU T1
+　　　　　　FULL OUTER JOIN
+　　　　　　　　　　　WAKUINFO T2
+　　　　　　ON　
+   　　　　　T2.HOSOWAKUID = T1.HOSOWAKUID
+　　　　　　AND
+   　　　　　T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
+　　　　　　WHERE
+　　　　　　　　　　　T1.UPDATEDATE >= (sysdate - INTERVAL '1' HOUR)
+　　　　　　AND
+　　　　　　　　　　　T2.WAKUSTATUS = 7
+　　　　　　ORDER BY
+　　　　　　　　　　　T1.HOSOWAKUID
+　　　　　　　　　　　,T1.HOSOWAKURIREKIID)A
 FULL OUTER JOIN
   (SELECT DISTINCT
-					T1.HOSOWAKUID         
-					,T1.HOSOWAKURIREKIID  
-					,T1.CURRENTFLAG       
-					,T1.KIKAKUID         
-					,T1.HOSODANID         
-					,T1.TYPEID            
-					,T2.WAKUSTATUS        
-			FROM
-			  HOSOWAKU T1
-			FULL OUTER JOIN
-			  WAKUINFORIREKI T2
-			ON
-			  T2.HOSOWAKUID = T1.HOSOWAKUID
-			AND
-			  T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
-			WHERE
-			  T1.HOSOWAKUID IN
-						(SELECT
-						   T1.HOSOWAKUID 
-						 FROM 
-						   HOSOWAKU T1 
-						 FULL OUTER JOIN
-						   WAKUINFO T2
-						 ON
-						   T2.HOSOWAKUID = T1.HOSOWAKUID
-						 AND
-						   T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
-						 WHERE 
-						   T1.UPDATEDATE >= (sysdate - INTERVAL '1' HOUR) 
-						 AND 
-						   T2.WAKUSTATUS = 7)
-			AND 
-			  T2.WAKUSTATUS = 5
-			ORDER BY
-			  T1.HOSOWAKUID
-			  ,T1.HOSOWAKURIREKIID)B
+   　　　　T1.HOSOWAKUID         
+　　　　　　　　　　,T1.HOSOWAKURIREKIID  
+　　　　　　　　　　,T1.CURRENTFLAG       
+　　　　　　　　　　,T1.KIKAKUID         
+　　　　　　　　　　,T1.HOSODANID         
+　　　　　　　　　　,T1.TYPEID            
+　　　　　　　　　　,T2.WAKUSTATUS        
+　　　　　　FROM
+　　　　　　　　　　HOSOWAKU T1
+　　　　　　FULL OUTER JOIN
+　　　　　　　　　　WAKUINFORIREKI T2
+　　　　　　ON
+　　　　　　　　 T2.HOSOWAKUID = T1.HOSOWAKUID
+　　　　　　AND
+　　　　　　　　　　T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
+　　　　　　WHERE
+　　　　　　　　　　T1.HOSOWAKUID IN
+	　　(SELECT
+	　　　　　　　　T1.HOSOWAKUID 
+	　　　　FROM 
+	　　　　　　　　HOSOWAKU T1 
+	　　　　FULL OUTER JOIN
+	　　　　　　 WAKUINFO T2
+	　　　　ON
+	　　　　　　　　T2.HOSOWAKUID = T1.HOSOWAKUID
+	　　　　AND
+	　　　　　　　　T2.HOSOWAKURIREKIID = T1.HOSOWAKURIREKIID
+	　　　　WHERE 
+	　　　　　　　　T1.UPDATEDATE >= (sysdate - INTERVAL '1' HOUR) 
+	　　　　AND 
+	　　　　　　　　T2.WAKUSTATUS = 7)
+　　　　　　　AND 
+　　　　　　　　　　T2.WAKUSTATUS = 5
+　　　　　　　ORDER BY
+　　　　　　　　　　T1.HOSOWAKUID
+　　　　　　　　　　,T1.HOSOWAKURIREKIID)B
 ON
   A.HOSOWAKUID = B.HOSOWAKUID
 WHERE
